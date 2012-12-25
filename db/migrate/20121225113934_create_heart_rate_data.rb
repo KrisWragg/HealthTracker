aa61c0836c84a5a49c936554bd94c739833cd6d8
saasbook <saasbook@saasbook>
2012-12-25 04:16:51 -0800

    testing commit...

--- db/migrate/20121225113934_create_heart_rate_data.rb
+++ db/migrate/20121225113934_create_heart_rate_data.rb
@@ -1,8 +1,10 @@
 class CreateHeartRateData < ActiveRecord::Migration
-  def change
-    create_table :heart_rate_data do |t|
-
-      t.timestamps
-    end
-  end
+	def change
+		create_table :heart_rate_data do |t|
+      date :datetime
+			resting_heart_rate :integer
+			
+			t.timestamps
+		end
+	end
 end
